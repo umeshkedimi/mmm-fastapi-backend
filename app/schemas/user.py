af4d259faf663755eb2b90a6da5236b2541e6a67
umeshkedimi <umesh836@gmail.com>
2025-06-06 12:54:55 +0530

    Added schemas/user.py file

---
+++ app/schemas/user.py
@@ -0,0 +1,11 @@
+# app/schemas/user.py
+
+from pydantic import BaseModel, EmailStr
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
